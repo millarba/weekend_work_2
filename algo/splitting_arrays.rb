f526a56afb4dd0e178c1d893c8b4ece75602fe30
millarba <millarba@gmail.com>
2017-01-21 14:33:37 -0600

    solved assignments

--- algo/splitting_arrays.rb
+++ algo/splitting_arrays.rb
@@ -1,7 +1,12 @@
 # Complete the method called split_array, which two parameters, the first being an array, and the second being an integer. The method will then create an array of other arrays, each one being the size of the indicated by the second parameter. See the driver code for examples.
 
 def split_array(array, array_size)
-
+  result = array
+  answer = []
+  while result.length >= array_size
+    answer << result.slice!(0..(array_size-1))
+  end
+  answer
 end
 
 # Driver code - don't touch anything below this line.
